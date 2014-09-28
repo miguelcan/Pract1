eb86d6740b27a827a9a54ffd9fcb2c404b522f1a
mike <mike@Mac-Pro-de-mike.local>
2014-09-27 23:56:16 -0700

    Initial Commit

---
+++ Lab01Tests/Lab01Tests.m
@@ -0,0 +1,40 @@
+//
+//  Lab01Tests.m
+//  Lab01Tests
+//
+//  Created by mike on 27/09/14.
+//  Copyright (c) 2014 mike. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface Lab01Tests : XCTestCase
+
+@end
+
+@implementation Lab01Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
