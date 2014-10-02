dff4d450e69d4738729b5d9484707e7ee80d9dba
mike <mike@Mac-Pro-de-mike.local>
2014-10-02 16:02:52 -0700

    scr4

---
+++ Lab01/Segm.h
@@ -0,0 +1,26 @@
+//
+//  Segm.h
+//  Lab01
+//
+//  Created by mike on 02/10/14.
+//  Copyright (c) 2014 mike. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Segm : UIViewController
+
+@property (strong, nonatomic) IBOutlet UISegmentedControl *segmTeam;
+
+@property (strong, nonatomic) IBOutlet UIImageView *imgTeam;
+
+@property (strong, nonatomic) IBOutlet UIStepper *stpGoal;
+
+@property (strong, nonatomic) IBOutlet UILabel *lblGoal;
+
+- (IBAction)evtSegm:(id)sender;
+
+- (IBAction)evtStep:(id)sender;
+
+
+@end
