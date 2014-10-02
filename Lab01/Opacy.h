5fc6ef7da0072f34cb10492ad62d09804448a97b
mike <mike@Mac-Pro-de-mike.local>
2014-10-02 12:21:39 -0700

    second screen

---
+++ Lab01/Opacy.h
@@ -0,0 +1,13 @@
+//
+//  Opacy.h
+//  Lab01
+//
+//  Created by mike on 02/10/14.
+//  Copyright (c) 2014 mike. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Opacy : UIViewController
+
+@end
--- Lab01/Opacy.m
+++ Lab01/Opacy.m
@@ -1,18 +1,18 @@
 //
-//  Transp.m
+//  Opacy.m
 //  Lab01
 //
 //  Created by mike on 02/10/14.
 //  Copyright (c) 2014 mike. All rights reserved.
 //
 
-#import "Transp.h"
+#import "Opacy.h"
 
-@interface Transp ()
+@interface Opacy ()
 
 @end
 
-@implementation Transp
+@implementation Opacy
 
 - (void)viewDidLoad {
     [super viewDidLoad];
